c573d87d5c9640e29e1e33db4700725a3c04e806
hirwaterry <hirwa.terry@gmail.com>
2025-08-13 09:10:37 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-black h-screen text-white">
+      <h1>hello</h1>
+    
+    </main>
+  );
+}
